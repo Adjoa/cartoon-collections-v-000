730be7e89d948175c520e16dffd6334eb6ff51fb
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-24 22:28:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,9 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteers)
-  planeteers.collect {|planeteer| "#{planeteer.capitalize}!"}
+  planeteers.collect do |planeteer|
+    "#{planeteer.capitalize}!"
+  end
 end
 
 def long_planeteer_calls(planeteer_calls)
